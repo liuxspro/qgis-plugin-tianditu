6a7a980122c853988ff6c4bdde8c056f7cb662d8
liuxspro <liuxspro@gmail.com>
2025-05-14 20:41:08 +0800

    Fix: remove duplicated initGui

--- tianditu_lite/plugin.py
+++ tianditu_lite/plugin.py
@@ -6,21 +6,20 @@ from .provider import TiandituProvider
 class TianDiTuLite:
     def __init__(self, iface):
         self.iface = iface
-        self.initGui()
+        self.tianditu_provider = TiandituProvider(self.iface)
 
     def initGui(self):
         self.registProvider()
 
     def registProvider(self):
-        """注册自定义的 QgsDataItemProvider"""
-        self.removeProvider()
-        t = TiandituProvider(self.iface)
-        QgsApplication.instance().dataItemProviderRegistry().addProvider(t)
+        QgsApplication.instance().dataItemProviderRegistry().addProvider(
+            self.tianditu_provider
+        )
 
     def removeProvider(self):
-        p = QgsApplication.instance().dataItemProviderRegistry().provider("Tianditu")
-        if p:
-            QgsApplication.instance().dataItemProviderRegistry().removeProvider(p)
+        QgsApplication.instance().dataItemProviderRegistry().removeProvider(
+            self.tianditu_provider
+        )
 
     def unload(self):
         """Unload from the QGIS interface"""
